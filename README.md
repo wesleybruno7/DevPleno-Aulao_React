3c4aabfebc39679efae7d704133f8b937bb640a4
Wesley Bruno Fernandes <wesleybruno7@terra.com.br>
2020-02-08 09:43:42 -0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# DevPleno-Aulao_React
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [estado, setEstado] = useState("Entrada");
+
+  const [palpite, setPalpite] = useState(150);
+  const [numPalpites, setNumPalpites] = useState(1);
+
+  const [min, setMin] = useState(0);
+  const [max, setMax] = useState(300);
+
+  const iniciarJogo = () => {
+    setEstado("Rodando");
+    setMin(0);
+    setMax(300);
+    setNumPalpites(1);
+    setPalpite(150);
+  };
+
+  if (estado === "Entrada") {
+    return <button onClick={iniciarJogo}>Começar a jogar</button>;
+  }
+
+  const menor = () => {
+    setNumPalpites(numPalpites + 1);
+    setMax(palpite);
+    const proxPalpite = parseInt((palpite - min) / 2) + min;
+    setPalpite(proxPalpite);
+  };
+
+  const maior = () => {
+    setNumPalpites(numPalpites + 1);
+    setMin(palpite);
+    const proxPalpite = parseInt((max - palpite) / 2) + palpite;
+    setPalpite(proxPalpite);
+  };
+
+  const acertou = () => {
+    setEstado("Fim");
+  };
+
+  if (estado === "Fim") {
+    return (
+      <div>
+        <p>
+          Acertei o número {palpite} com {numPalpites} chutes!
+        </p>
+        <button onClick={iniciarJogo}>Jogar novamente!</button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="App">
+      <p>O seu número é {palpite}?</p>
+      <button onClick={menor}>Menor!</button>
+      <button onClick={acertou}>Acertou!</button>
+      <button onClick={maior}>Maior!</button>
+    </div>
+  );
+}
